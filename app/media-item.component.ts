e6ba55fd2b23b588cc73c1cf877df377637cdcf4
MartianPegasus <martianpegasus@gmail.com>
2019-09-05 13:28:20 +0200

    till forms we got

--- app/media-item.component.ts
+++ app/media-item.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'mw-media-item',
   templateUrl: 'app/media-item.component.html',
   styleUrls: ['app/media-item.component.css']
 })
-export class MediaItemComponent { }
+export class MediaItemComponent { 
+  @Input() mediaItem;
+  @Output() delete = new EventEmitter();
+
+  onDelete(){
+    this.delete.emit(this.mediaItem);
+    console.log('Deleted');
+  }
+
+}
